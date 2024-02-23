a036c684f300cfc2b97e1c497119c9a9703996fc
ecoddr3 <ecoddr3@gmail.com>
2024-02-23 13:43:23 +0900

    更新

---
+++ Assets/Utage/Scripts/ADV/Custom/CustomData/AdvCustomDataSettings.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Utage
+{
+    [Serializable]
+    public class AdvCustomDataSettings
+    {
+        List<AdvCustomDataContainerCreator> DataContainerCreators => dataContainerCreators;
+        [SerializeField] List<AdvCustomDataContainerCreator> dataContainerCreators;
+
+        public bool IsCustomData(string customDataName)
+        {
+            return FindDataContainerCreator(customDataName) != null;
+        }
+
+        public AdvCustomDataContainerCreator FindDataContainerCreator(string customDataName)
+        {
+            return DataContainerCreators.Find(x => x.IsTargetDataName(customDataName));
+        }
+    }
+}
